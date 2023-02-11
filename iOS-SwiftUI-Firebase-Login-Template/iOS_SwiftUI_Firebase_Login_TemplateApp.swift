f714ce4be30e40a693ca11cbfb9b845ec4b0bd55
Tanner Maasen <tmaasen@gmail.com>
2023-02-11 01:54:09 -0600

    Initial Commit

---
+++ iOS-SwiftUI-Firebase-Login-Template/iOS_SwiftUI_Firebase_Login_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_SwiftUI_Firebase_Login_TemplateApp.swift
+//  iOS-SwiftUI-Firebase-Login-Template
+//
+//  Created by Tanner Maasen on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_SwiftUI_Firebase_Login_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
